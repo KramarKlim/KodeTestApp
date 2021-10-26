a0fa5bcf9d1b41ca822d538268b1a757b419dbf5
KramarKlim <kramarklim@yandex.ru>
2021-10-26 21:53:52 +0200

    Initial Commit

---
+++ KodeTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KodeTestApp
+//
+//  Created by Клим on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
